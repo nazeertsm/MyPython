c13a203737c524e27b31f944ab5e3eabc6c2ec67
ahmedna <nazeer.ahmed@navis.com>
2022-11-19 20:27:19 +0530

    Basic_If condition

---
+++ ControlFlow/Ifst1.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+    print("you can ride the rollercoaster")
+else:
+    print("you cannot ride the rollercoaster")
