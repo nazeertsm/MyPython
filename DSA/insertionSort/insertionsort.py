e763f5d4cb3b1b3f7fdee1deba11370603150d5f
ahmedna <nazeer.ahmed@navis.com>
2022-11-25 18:52:20 +0530

    insertionSort

---
+++ DSA/insertionSort/insertionsort.py
@@ -0,0 +1,18 @@
+## Implementation of Insertion Sort
+## Time Complexity: O(n^2)
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i-1
+        key = arr[i]
+        
+        while j>=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+
+## Driver code
+arr = [75, 90, 100, 95, 85, 80]
+result = insertion_sort(arr)
+print("Sorted array after applying the insertion sort:", result)
